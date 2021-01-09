f89f4a6a5807036b905f1aa4be0d1c3e02243c12
Esteban Mathia <esteban.mathia@supinfo.com>
2021-01-09 23:57:22 +0100

    Backward update avant test

--- ant.py
+++ ant.py
@@ -15,17 +15,31 @@ class Ants():
 		#white go to the rigth but not white go to the left
 		self._orientation += 1 if color_of_the_case == "white" else -1 #if it's white then +1 else -1
 
+		self.update()
+
+		if color_of_the_case == "white":
+			return self._color
+		else :
+			return 'white'
+
+	def bchange_direction(self, color_of_the_case):
+		#white go to the rigth but not white go to the left
+		self._orientation += -1 if color_of_the_case == "white" else 1 #if it's white then -1 else +1
+
+		self.update()
+
+		if color_of_the_case == "white":
+			return 'white'
+		else :
+			return self._color
+
+	def update(self):
 		if self._orientation < 0:
 			self._orientation += 4
 		elif self._orientation >3:
 			self._orientation -= 4
 
 		self._current_direction = self.direction[self._orientation]
-
-		if color_of_the_case == "white":
-			return self._color
-		else :
-			return 'white'
 
 	def set_coordonate(self, x, y):
 		self._coordonate = [x, y]
@@ -56,6 +70,25 @@ class New_Ants():
 		#white go to the rigth but not white go to the left
 		self._orientation += 1 if color_of_the_case == "white" else -1 #if it's white then +1 else -1
 
+		self.update()
+
+		if color_of_the_case == "white":
+			return self._color
+		else :
+			return 'white'
+
+	def bchange_direction(self, color_of_the_case):
+		#white go to the rigth but not white go to the left
+		self._orientation += -1 if color_of_the_case == "white" else 1 #if it's white then -1 else +1
+
+		self.update()
+
+		if color_of_the_case == "white":
+			return 'white'
+		else :
+			return self._color
+
+	def update(self):
 		if self._orientation < 0:
 			self._orientation += 4
 		elif self._orientation >3:
@@ -63,10 +96,13 @@ class New_Ants():
 
 		self._current_direction = self.direction[self._orientation]
 
-		if color_of_the_case == "white":
-			return self._color
-		else :
-			return 'white'
+	def update(self):
+		if self._orientation < 0:
+			self._orientation += 4
+		elif self._orientation >3:
+			self._orientation -= 4
+
+		self._current_direction = self.direction[self._orientation]
 
 	def set_coordonate(self, x, y):
 		self._coordonate = [x, y]
